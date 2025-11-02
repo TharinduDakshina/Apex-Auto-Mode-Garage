f5fa626580f2017f4d635d5cb439832732ff94ac
TharinduDakshina <tharindudakshina527@gmail.com>
2025-11-02 19:55:02 +0530

    init apex auto mode garage backend

---
+++ src/main/java/com/apex_auto_mode_garage/apexAutoModeGarage/ApexAutoModeGarageApplication.java
@@ -0,0 +1,13 @@
+package com.apex_auto_mode_garage.apexAutoModeGarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexAutoModeGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexAutoModeGarageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex_auto_mode_garage/apexAutoModeGarage/ApexAutoModeGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex_auto_mode_garage.apexAutoModeGarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexAutoModeGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
